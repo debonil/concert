c71cdb68b91f8822d4ba514be441b8240137487b
Ankit Pandey <ankit.pandey@cris.org.in>
2018-01-08 15:15:45 +0530

    refs #201801081515

--- src/app/enquiries/time-table/time-table.component.ts
+++ src/app/enquiries/time-table/time-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms/src/model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-time-table',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-table.component.scss']
 })
 export class TimeTableComponent implements OnInit {
+  accomodationAvailabilityFetched: boolean;
+  accomodationAvailabilityForm: FormGroup;
 
-  constructor() { }
+  inputTrainNo: FormControl;
+  inputTrainName: FormControl;
+
+  fetchedAccomodationAvailability: any = [
+    {stn_Code: 'NDLS', stn_Name: 'New Delhi', arrival_Time: 'Source', departure_Time: '18:55', distance: '0', day: '1' },
+    {stn_Code: 'CNB', stn_Name: 'New Delhi', arrival_Time: '00:40', departure_Time: '00:55', distance: '440', day: '2' },
+    {stn_Code: 'ALD', stn_Name: 'New Delhi', arrival_Time: '03:20', departure_Time: '03:45', distance: '634', day: '2' },
+    {stn_Code: 'MUV', stn_Name: 'New Delhi', arrival_Time: '07:55', departure_Time: 'Destination', distance: '755', day: '2' }
+  ];
+
+  todayDate: Date = new Date();
+  stnList:  any = [
+    {stnCode: 'NDLS', stnName: 'New Delhi'},
+    {stnCode: 'CNB', stnName: 'Kanpur Central'},
+    {stnCode: 'ALD', stnName: 'Allahabad Jn'},
+    {stnCode: 'MUV', stnName: 'Manduadih'},
+  ];
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+    this.inputTrainNo = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]);
+    this.inputTrainName = new FormControl({value: '', disabled: true}, [Validators.required]);
+    this.accomodationAvailabilityForm =  this.fb.group({
+      inputTrainNo: this.inputTrainNo,
+      inputTrainName: this.inputTrainName
+    });
+  }
 
   ngOnInit() {
   }
 
+  getInputTrainNoErrorMessage() {
+    return this.inputTrainNo.hasError('required') ? 'You must enter a value' : 'Enter a valid Train No.';
+  }
+
+  onlyNumericInput(e:any) {
+    let key: any;
+    key = ((document.all) ? (key = e.keyCode) : (key = e.which));
+    return (key === 8 || key === 32 || (key >= 48 && key <= 57));
+  }
+
+  inputTrainNoChangeFn(e: KeyboardEvent) {
+    // console.log('e: ' + e.key);
+
+    if (this.inputTrainNo.value.length === 5) {
+      this.inputTrainName.setValue('SHIV GANGA EXP');
+    }
+  }
+
+  fetchAccomodationAvailability() {
+    if (this.accomodationAvailabilityForm.valid) {
+      this.snackBar.open('Accomodation Availability Fetched!', '', {
+        duration: 3000,
+      });
+
+      this.accomodationAvailabilityFetched = true;
+    } else {
+      this.snackBar.open('Please fill the form appropriately!', '', {
+        duration: 4000,
+      });
+    }
+  }
 }
