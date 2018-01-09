55e6ca3190fa052995f9139712305351f8163190
Ankit Pandey <ankit.pandey@cris.org.in>
2018-01-09 18:11:17 +0530

    refs #201801091811

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,32 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent, ThemeDialogComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { JourneyplannerModule } from './journeyplanner/journeyplanner.module';
 import { EnquiriesModule } from './enquiries/enquiries.module';
 import { ServicesModule } from './services/services.module';
 import { DtcTxnComponent } from './dtc-txn/dtc-txn.component';
+import { MyMaterialComponentsModule } from './my-material-components/my-material-components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    HeaderComponent, ThemeDialogComponent,
     FooterComponent,
     DtcTxnComponent
   ],
+  entryComponents: [ThemeDialogComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     ServicesModule,
+    FormsModule, ReactiveFormsModule,
+    MyMaterialComponentsModule,
     JourneyplannerModule,
     EnquiriesModule
   ],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,39 @@ export class HeaderComponent implements OnInit {
   @Input() themeIn: string;
   @Output() themeOut = new EventEmitter();
 
-  constructor() { }
+  dialogRef: any;
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  openDialog(): void {
+    this.dialogRef = this.dialog.open(ThemeDialogComponent, {
+      // width: '8.5rem',
+      data: { theme: this.themeIn }
+    });
+    // dialogRef.updatePosition({ top: '50px', right: '10px' });
+    this.dialogRef.afterClosed().subscribe(theme => {
+      console.log('The dialog was closed' + theme);
+      if (theme !== undefined) {
+        this.themeOut.emit(theme);
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'app-theme-dialog',
+  templateUrl: 'theme-dialog.component.html'
+})
+export class ThemeDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ThemeDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  changeTheme(theme: string): void {
+    this.data.theme = theme;
+    this.dialogRef.close(this.data.theme);
+  }
 }
