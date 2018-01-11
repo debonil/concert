21b45dae0543095f6be92eae8933baef13c29d09
Ankit Pandey <ankit.pandey@cris.org.in>
2018-01-11 17:27:08 +0530

    refs #201801111727

--- src/app/enquiries/enquiries.component.ts
+++ src/app/enquiries/enquiries.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-enquiries',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnquiriesComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('focusSetter') focusSetter: any;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.router.events
+    .subscribe((event: any) => {
+      console.log(`event: ${(event)}`);
+      if (event instanceof NavigationEnd) {
+        this.focusSetter.nativeElement.focus();
+      }
+    })
   }
 
 }
